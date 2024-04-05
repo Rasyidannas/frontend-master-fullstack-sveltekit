947a485de43e5d80cdabd98916694600511dfcea
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-05 09:08:27 +0700

    1/ 1. routing in svelte & 2. loading data on the server.mp4

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    // this will called data from load function in +page.server.js
+    export let data;
+</script>
+
+<h1>blog</h1>
+
+<ul>
+    {#each data.summaries as {slug, title}}
+        <li>
+            <a href={`blog/${slug}`}>{title}</a>
+        </li>
+    {/each}
+</ul>
---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    // this will called data from load function in +page.server.js
+    export let data;
+</script>
+
+<h1>{data.post.title}</h1>
+<div>
+    <!-- @html is for sanitize html -->
+    {@html data.post.content}
+</div>
