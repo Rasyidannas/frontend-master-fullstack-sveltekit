97af3926088fb750bb7cbd00a1a50d4be3ae3349
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-05 10:44:05 +0700

    1/4. headers cookies shared modules part 3.mp4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     //this will get data from load function in +page.server.js
     export let data;
+    import { message } from '$lib/message.js';
 </script>
 
 <h1>Hello {data.visited ? 'friend' : 'stranger'}!</h1>
+<p>{message}</p>
