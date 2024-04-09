84b4a3c4a60dca7a208eb7994b34a85cd282a458
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-09 08:50:30 +0700

    fix: 1/11. sveltekit store.mp4

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,14 @@ import adapter from "@sveltejs/adapter-auto";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // ideally, this should be something deterministic
-    // like the output of `git rev-parse HEAD`
-    name: Date.now().toString(),
+    version: {
+      // ideally, this should be something deterministic
+      // like the output of `git rev-parse HEAD`
+      name: Date.now().toString(),
 
-    // if undefined, no polling will occur
-    pollInterval: 5000,
+      // if undefined, no polling will occur
+      pollInterval: 5000,
+    },
 
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
