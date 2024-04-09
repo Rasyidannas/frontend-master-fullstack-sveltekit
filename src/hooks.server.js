4ed2c47323a262e1c95839eb3732b1bf85b12d0a
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-09 09:40:54 +0700

    2/13. hooks part 2.mp4

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,12 +1,7 @@
 //hooks is ways to run code before or after a request is made to the server for intercept and override the framework's default behavior.
 
 export async function handle({ event, resolve }) {
-  if (event.url.pathname === "/ping") {
-    return new Response("pong");
-  }
+  event.locals.answer = 42; //this for send data to the route
 
-  return await resolve(event, {
-    transformPageChunk: ({ html }) =>
-      html.replace("<body", '<body style="color: hotpink"'),
-  });
+  return await resolve(event);
 }
