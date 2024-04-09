5ec6724ca060302b335086426c25add9798bd52b
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-09 12:34:05 +0700

    2/14. page options - ssr.mp4

---
+++ src/routes/+page.server.js
@@ -0,0 +1,3 @@
+//Server-side rendering (SSR) is the process of generating HTML on the server, and is what SvelteKit does by default. It's important for performance and resilience, and is very beneficial for search engine optimization (SEO) — while some search engines can index content that is rendered in the browser with JavaScript, it happens less frequently and reliably.
+
+export const ssr = false; //this will set SSR and not into SPA
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,1 @@
-<h1>home</h1>
+<h1>{window.innerWidth}x{window.innerHeight}</h1>
