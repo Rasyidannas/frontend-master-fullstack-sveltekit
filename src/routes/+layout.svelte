199f5a39a5a0c97b449a93d1d2f1e3f5e4afa0c2
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-09 15:32:09 +0700

    2/16. advanced routing - optional prameters.mp4

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,8 @@
-<script>
-	import { onMount } from 'svelte';
-
-	let seconds = 0;
-
-	onMount(() => {
-		const interval = setInterval(() => {
-			seconds += 1;
-		}, 1000);
-
-		return () => {
-			clearInterval(interval);
-		};
-	});
-</script>
-
-<!-- data-sveltekit-reload will make this page reload when go another route -->
-<nav data-sveltekit-reload>
-	<a href="/">home</a>
-	<a href="/about">about</a>
+<nav>
+	<a href="/">default</a>
+	<a href="/en">english</a>
+	<a href="/de">german</a>
+	<a href="/fr">french</a>
 </nav>
 
 <slot />
-
-<p>the page has been open for {seconds} seconds</p>
