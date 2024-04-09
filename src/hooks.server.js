b55c093bf0cd4724c0bf22a4760b6c194b5f286f
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-09 10:58:59 +0700

    2/13. hooks part 3.mp4

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,7 +1,11 @@
 //hooks is ways to run code before or after a request is made to the server for intercept and override the framework's default behavior.
 
-export async function handle({ event, resolve }) {
-  event.locals.answer = 42; //this for send data to the route
+export async function handleFetch({ event, request, fetch }) {
+  const url = new URL(request.url);
+  //in here we are responding to request /a/+server.js with /b/+server.js
+  if (url.pathname === "/a") {
+    return await fetch("/b");
+  }
 
-  return await resolve(event);
+  return await fetch(request);
 }
