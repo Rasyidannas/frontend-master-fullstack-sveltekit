2842b8df453f58aba907094add17c609fca91cd7
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-10 09:04:03 +0700

    2/17. advanced loading - using both load functions.mp4

---
+++ src/routes/+page.js
@@ -0,0 +1,12 @@
+//+page.js will load data from +page.server.js
+
+export async function load({ data }) {
+  const module = data.cool
+    ? await import("./CoolComponent.svelte")
+    : await import("./BoringComponent.svelte");
+
+  return {
+    component: module.default,
+    message: data.message,
+  };
+}
